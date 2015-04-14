98531a7e2c39a5a502ba7f14e2923c848d78eb40
Ed Lea <edleadesign@gmail.com>
2015-04-13 20:16:10 -0700

    initial commit

---
+++ Contents/Sketch/app.js
@@ -0,0 +1,193 @@
+/*
+ * TODO
+ * - Add notifications
+ * - Find a non-blocking way to send all artboards
+ * - Work through a selection
+ */
+
+
+
+
+
+var exportAll = function(context){
+    var doc = context.document;
+    var selection = context.selection;
+    var pages = getAllPages(context);
+}
+
+function requestUri(){
+    var task = NSTask.alloc().init()
+    task.setLaunchPath("/usr/bin/curl");
+    var args = NSArray.arrayWithObjects(
+        "-v",
+        "-X", "GET",
+        "--header", "User-Agent: Sketch",
+        "http://127.0.0.1:8000/app_dev.php/request/id", nil);
+    task.setArguments(args);
+
+    var outputPipe = [NSPipe pipe];
+    [task setStandardOutput:outputPipe];
+
+    task.launch();
+
+    var outputData = [[outputPipe fileHandleForReading] readDataToEndOfFile];
+    var outputString = [[[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding]];
+    return JSON.parse(outputString);
+}
+
+function getAllPages(context){
+
+    var doc = context.document;
+    doc.showMessage("Uploading Artboards...");
+    /*
+     * Set up initial variables
+     */
+    var pages       = doc.pages(),
+        pageCount   = pages.count(),
+        tempDir     = NSTemporaryDirectory(),
+        json        = requestUri(),
+        update      = false;
+
+    var uniqueId    = String( json.id );
+    var version     = String( json.version ); // plugin version
+
+    /*
+     * Check if this plugin version is up to date,
+     * if not, send an update reminder.
+     *
+     */
+    if( version > 0.1 ){
+        // this means the plugin is out of date.
+        // help the user update the plugin
+        update = true;
+    }
+
+
+    for (index = 0; index < pageCount; ++index) {
+        var page = pages[index];
+
+        /*
+         * We have to set the current page of the document otherwise the png for the artboard will export empty
+         */
+        doc.setCurrentPage(page);
+        var artboards = page.artboards();
+        var artboardsCount = artboards.count();
+        if (artboardsCount >= 1){
+            for (i = 0; i < artboardsCount; ++i) {
+                var artboard = artboards[i];
+                var filename = safeName(artboard.name()) + ".png";
+                var path = tempDir + filename;
+                [doc saveArtboardOrSlice:artboard toFile:path];
+                var result = post(path, filename, uniqueId);
+                doc.showMessage("Completed page " + (index+1) + " of " + pageCount);
+                log( "this artboard " + artboard.name() + " is on " + page.name() + " in " + path )
+            }
+        }
+    }
+
+    var url = "http://127.0.0.1:8000/app_dev.php" + "/view/" + uniqueId;
+
+    successWindow(context, url, update);
+//    dialog("Files Uploaded", "Visit: http://127.0.0.1:8000/app_dev.php" + "/view/" + uniqueId);
+
+}
+
+
+function post(path, filename, uniqueId){
+    var task = NSTask.alloc().init()
+    task.setLaunchPath("/usr/bin/curl");
+    var args = NSArray.arrayWithObjects(
+        "-v",
+        "-X", "POST",
+        "--header", "User-Agent: Sketch",
+        "Content-Disposition: form-data; name=artboardfile; Content-Type=image/png;",
+        "--form", "sketchartboard=@" + path,
+        "--form", "filename=" + filename,
+        "--form", "id=" + uniqueId,
+        "http://127.0.0.1:8000/app_dev.php/upload/sketch", nil);
+    task.setArguments(args);
+
+    var outputPipe = [NSPipe pipe];
+    [task setStandardOutput:outputPipe];
+
+    task.launch();
+
+    var outputData = [[outputPipe fileHandleForReading] readDataToEndOfFile];
+    var outputString = [[[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding]]; // Autorelease optional, depending on usage.
+
+    return JSON.parse(outputString);
+}
+
+
+
+
+function safeName(filename){
+    return filename.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+}
+
+function dialog(title,text){
+    var app = [NSApplication sharedApplication];
+    [app displayDialog:text withTitle:title]
+}
+
+function successWindow(context, viewURL, update){
+
+
+    // create the enclosing window
+    var win     = NSWindow.alloc().init();
+    [win setFrame:NSMakeRect(0, 0, 320, 200) display:false]
+
+
+    // create the buttons wrapper
+    var buttonWrapper = [[NSView alloc] initWithFrame:NSMakeRect(0, 0, 320, 150)];
+        buttonWrapper.setWantsLayer(true);
+        [[win contentView] addSubview:buttonWrapper];
+
+
+
+    // create the done button
+    var doneButton = [[NSButton alloc] initWithFrame:NSMakeRect(0, 0, 92, 32)]
+    [doneButton setTitle:"Done"]
+    [doneButton setBezelStyle:NSRoundedBezelStyle]
+    [doneButton setCOSJSTargetFunction:function(sender) {
+        [win orderOut:nil]
+        [NSApp stopModal]
+    }];
+    [doneButton setAction:"callAction:"]
+    [buttonWrapper addSubview:doneButton]
+
+
+    if ( update ){
+        // create the update button
+        var updateRect = NSMakeRect(50, 0, 160, 32);
+        var updateButton = NSButton.alloc().initWithFrame(updateRect);
+        updateButton.setTitle("Updates Available");
+        [updateButton setBezelStyle:NSRoundedBezelStyle]
+        [updateButton setCOSJSTargetFunction:function(sender) {
+            var updateUrl = [NSURL URLWithString:@"http://bbc.co.uk/"];
+            if( ![[NSWorkspace sharedWorkspace] openURL:updateUrl] ){
+                sketchLog(@"Could not open url:" + [updateUrl description])
+            }
+        }];
+        [updateButton setAction:"callAction:"]
+        [buttonWrapper addSubview:updateButton]
+    }
+
+
+    // create the URL button
+    var rect = NSMakeRect(150, 0, 160, 32);
+    var linkButton = NSButton.alloc().initWithFrame(rect);
+        linkButton.setTitle("Click to view link");
+        [linkButton setBezelStyle:NSRoundedBezelStyle]
+        [linkButton setCOSJSTargetFunction:function(sender) {
+            var url = NSURL.URLWithString(String( viewURL ));
+            if( ![[NSWorkspace sharedWorkspace] openURL:url] ){
+                sketchLog(@"Could not open url:" + [url description])
+            }
+        }];
+        [linkButton setAction:"callAction:"]
+        [buttonWrapper addSubview:linkButton]
+
+
+    [NSApp runModalForWindow:win]
+}
